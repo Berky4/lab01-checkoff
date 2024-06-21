6946eb7c34836a40eefe3c3d57d3f1f9daff5e84
Berky4 <93950927+Berky4@users.noreply.github.com>
2024-06-21 06:50:52 +0530

    Update Arithmetic.java

--- lab01/src/Arithmetic.java
+++ lab01/src/Arithmetic.java
@@ -6,15 +6,4 @@ public class Arithmetic {
     public int subtract(int a, int b) {
         return a - b;
     }
-    
-    public int multiply(int a, int b) {
-        return a * b;
-    }
-    
-    public double divide(int a, int b) {
-        if (b == 0) {
-            throw new IllegalArgumentException("Cannot divide by zero");
-        }
-        return (double) a / b;
-    }
 }
